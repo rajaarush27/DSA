fd5fc41d96aa8c9663b26e1826a2261593af2bff
Aarush <aarushraj034@gmail.com>
2026-02-21 13:41:40 +0530

    Linked List Leetcode

---
+++ src/main/java/com/cb/linkedlist/LC_160_IntersactionOfTwoLinkedList.java
@@ -0,0 +1,36 @@
+package com.cb.linkedlist;
+
+public class LC_160_IntersactionOfTwoLinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public class Solution {
+		public ListNode getIntersactionNode(ListNode headA, ListNode headB) {
+			ListNode a = headA;
+			ListNode b = headB;
+
+			while (a != b) {
+				if (a == null) {
+					a = headB;
+				} else {
+					a = a.next;
+				}
+				if (b == null) {
+					b = headA;
+				} else {
+					b = b.next;
+				}
+			}
+			return a;
+		}
+	}
+
+}
